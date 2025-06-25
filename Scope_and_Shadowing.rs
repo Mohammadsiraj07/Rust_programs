3b2e8d1fd87c3a00f68f61c23cf02d745829629e
Mohammadsiraj07 <siraj637900@gmail.com>
2025-06-25 11:04:00 +0530

    Update Scope_and_Shadowing.rs

--- Scope_and_Shadowing.rs
+++ Scope_and_Shadowing.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let x = 50; // outer scope
-
+    let x = 50;
     {
-        let x = 100; // shadows outer x
+        let x = 100; 
         println!("Inside block, x = {}", x);
     }
-
     println!("Outside block, x = {}", x);
 }
