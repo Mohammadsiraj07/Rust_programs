d652a52e10a9d2b7a24d78a577b5af9b7fd65a42
Mohammadsiraj07 <siraj637900@gmail.com>
2025-06-25 11:10:34 +0530

    Update Implicit_and_Explicit_type_declaration.rs

--- Implicit_and_Explicit_type_declaration.rs
+++ Implicit_and_Explicit_type_declaration.rs
@@ -1,3 +1,6 @@
+/*Write a program to implement the following
+a. Implicit type declaration
+b. Explicit type declaration*/
 fn main() {
     let a = 10.5;
     println!("Implicit: a = {} | type = f64", a);
