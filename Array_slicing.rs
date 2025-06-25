0f9e8cdb861501274b681d3e3f850df23b6eb7a6
Mohammadsiraj07 <siraj637900@gmail.com>
2025-06-25 11:05:50 +0530

    Update Array_slicing.rs

--- Array_slicing.rs
+++ Array_slicing.rs
@@ -1,15 +1,12 @@
+/* Write a program to create an array of 10 elements and implement the following
+a. Create a of 2nd and 3rd element
+b. Omit the start index of the slice
+c. Omit the End Index of the Slice
+d. Omit both Start and End Index of the Slice*/
 fn main() {
     let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    // a. Slice of 2nd and 3rd element
     println!("2nd and 3rd elements: {:?}", &arr[1..3]);
-
-    // b. Omit start index (upto 5th)
     println!("Omit start index (upto 5th): {:?}", &arr[..5]);
-
-    // c. Omit end index (from 5th)
     println!("Omit end index (from 5th): {:?}", &arr[5..]);
-
-    // d. Omit both start and end
     println!("Whole array using slice: {:?}", &arr[..]);
 }
